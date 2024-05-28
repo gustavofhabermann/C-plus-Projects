a8e9100d731d01adc177cb3e0821712f55453d69
gustavofhabermann <160959136+gustavofhabermann@users.noreply.github.com>
2024-05-27 21:42:05 -0300

    Update Ex5main.cpp
    Conserto do exe5

--- Ex5main.cpp
+++ Ex5main.cpp
@@ -10,7 +10,7 @@ int main() {
 	cout << "Me fale qual e a velocidade da sua internet em (Mbps)" << endl;
 	cin >> veloc_internet;
 	
-	time = size/(veloc_internet * 60);
+	time = size/((veloc_internet * 60) / 8);
 	
 	cout << "O tempo que vai demorar para baixar esse arquivo e de: " << time << " minutos";
 	return 0;
